1fe6ec6dd2887afc2193faf446faf70865162bc8
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-12 10:36:40 +0300

    register propertyslices to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/store/slices/authSlice';
+import propertyReducer from '@/store/slices/propertySlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    properties: propertyReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
