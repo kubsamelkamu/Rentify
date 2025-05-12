c1f8fb9463504eebb992dfb769cab2bed5d1be4c
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-12 10:41:42 +0300

    register Toaster in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import { store } from "@/store/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from 'react-redux';
-
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Toaster position="top-right" />
     </Provider>
   );
 }
