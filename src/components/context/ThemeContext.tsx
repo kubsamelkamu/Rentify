9a81864add2065198e2aee1d8957bd29fc2f61ec
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-14 10:01:43 +0300

    implemted ThemeContext

---
+++ src/components/context/ThemeContext.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState, ReactNode } from "react";
+
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [theme, setTheme] = useState<string>('light');
+
+    const toggleTheme = () => {
+        setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
