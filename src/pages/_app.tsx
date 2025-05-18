0e249a7707f8f118d05eeff38e19e3318b98ac4e
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-18 22:03:56 +0300

    update app component with SocketConnector

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,35 @@
-import { store } from "@/store/store";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import { Provider, useSelector } from 'react-redux';
+import { store, RootState } from '@/store/store';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from '@/components/context/ThemeContext';
+import { connectSocket } from '@/utils/socket';
+
+function SocketConnector() {
+
+  const token = useSelector((state: RootState) => state.auth.token);
+
+  useEffect(() => {
+    if (token) {
+      connectSocket(token);
+    }
+  }, [token]);
+
+  return null;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Provider store={store}>
+        <SocketConnector />
+        
         <Component {...pageProps} />
         <Toaster position="top-right" />
       </Provider>
     </ThemeProvider>
   );
 }
+
