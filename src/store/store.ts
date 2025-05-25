69ad621cfc27dc25c7e1c4ebbb187fe51e99690a
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-25 23:12:30 +0300

    register bookingSlice in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/store/slices/authSlice';
 import propertyReducer from '@/store/slices/propertySlice';
+import bookingReducer from '@/store/slices/bookingSlice';  
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     properties: propertyReducer,
+    bookings: bookingReducer,  
   },
 });
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch; 
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
