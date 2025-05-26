2ef2274ba4634bf61d52c2126b574083a3879490
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-26 12:07:56 +0300

    register manifest in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon.png" sizes="192x192" />
+        <link rel="icon" href="/icons/icon.png" type="image/png" />
+        <link rel="preload" href="/icons/icon.png" as="image" />
+        <meta name="theme-color" content="#2E86AB" />
+        <meta name="msapplication-TileColor" content="#2E86AB" />
+        <meta name="msapplication-TileImage" content="/icons/icon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
