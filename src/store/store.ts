d9ad5f86bfa8d4243a685031594f6c24cbf33d26
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-06-01 11:43:44 +0300

    add payment-slice to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/store/slices/authSlice';
 import propertyReducer from '@/store/slices/propertySlice';
 import bookingReducer from '@/store/slices/bookingSlice';  
+import paymentReducer from '@/store/slices/paymentSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     properties: propertyReducer,
     bookings: bookingReducer,  
+    payment: paymentReducer,
   },
 });
 
