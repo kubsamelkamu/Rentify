51138fcf909ef2706059b88d1e1925880c13054b
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-06-03 08:55:47 +0300

    add user profile-pic to user

--- src/components/userLayout/Layout.tsx
+++ src/components/userLayout/Layout.tsx
@@ -1,18 +1,25 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useContext } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ThemeContext } from "@/components/context/ThemeContext";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const UserLayout = ({ children }: LayoutProps) => {
+  const { theme } = useContext(ThemeContext)!;
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div
+      className={`flex flex-col min-h-screen ${
+        theme === "dark"
+          ? "bg-gray-900 text-gray-100"
+          : "bg-gray-50 text-gray-900"
+      }`}
+    >
       <Header />
-      <main className="flex-grow overflow-auto bg-gray-100">
-        {children}
-      </main>
+      <main className="flex-grow overflow-auto">{children}</main>
       <Footer />
     </div>
   );
